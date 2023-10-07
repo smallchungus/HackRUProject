bb549b7a7a8cac6655eced487d85dda88802f449
smallchungus <wchen1396@gmail.com>
2023-10-07 19:16:43 -0400

    making things more aesthetically pleasing
    need to fix the confirm button

---
+++ .history/hackruproject/src/views/HomeView_20231007182921.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="protein" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ .history/hackruproject/src/views/HomeView_20231007182939.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <img alt="protein" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
