28221460c91e53f84a928cd7cf2310897a5f97c6
fajrsoli <fms59@rutgers.edu>
2023-10-07 16:01:14 -0400

    login and signup
    login and signup

--- hackruproject/src/views/LoginView.vue
+++ hackruproject/src/views/LoginView.vue
@@ -1,6 +1,37 @@
 <template>
     <div class="login">
-      <h1>This is an login page</h1>
+    
+      <!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Login / Sign Up Form</title>
+    <link rel="shortcut icon" href="/assets/favicon.ico">
+    <link rel="stylesheet" href="./src/main.css">
+</head>
+<body>
+    <div class="container">
+        <form class="form" id="login">
+            <h1 class="form__title">Login</h1>
+            <div class="form__message form__message--error"></div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Username or email">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <button class="form__button" type="submit">Continue</button>
+            <p class="form__text">
+                <a class="form form--hidden" href="RegisterView.vue" id="login">Don't have an account? Register</a>
+            </p>
+        </form>
+        
+    </div>
+    
+</body>
+
     </div>
   </template>
   
--- hackruproject/src/views/RegisterView.vue
+++ hackruproject/src/views/RegisterView.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="register">
-      <h1>This is an register page</h1>
+      <form class="form form--hidden" id="createAccount">
+            <h1 class="form__title">Register</h1>
+            <div class="form__message form__message--error"></div>
+            <div class="form__input-group">
+                <input type="text" id="signupUsername" class="form__input" autofocus placeholder="Username">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Email Address">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Confirm password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <button class="form__button" type="submit">Continue</button>
+            <p class="form__text">
+                <a class="login" href="LoginView.vue" id="login">Already have an account? Sign in</a>
+            </p>
+        </form>
     </div>
   </template>
   
