0a3dd97e9911fcdee0754e80fdd0f52defe40771
fajrsoli <fms59@rutgers.edu>
2023-10-07 18:51:16 -0400

    Update HomeView.vue
    for now just general input boxes with maxlength will work on adding restrictions now

--- hackruproject/src/views/HomeView.vue
+++ hackruproject/src/views/HomeView.vue
@@ -1,11 +1,39 @@
 <template>
   <div class="home">
-    <img alt="protein" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="" src="">
+    <HelloWorld msg="Protein/Peptide Input"/>
   </div>
+
+<div class ="Protein">
+  <div class="multiline">
+    <span>Protein:</span>
+    <p style="white-space: pre-line;">{{ message }}</p>
+    <input type="text" class="form__input" autofocus placeholder="Input Your Protein Here" :maxlength=400 width="400px">
+  </div>
+  <button class="form__button" type="submit">Get My Results</button>
+            <p class="form__text">
+            </p>
+</div>
+
+
+<div class ="Peptide">
+  <div class="multiline">
+    <span>Peptide:</span>
+    <p style="white-space: pre-line;">{{ message }}</p>
+    <input type="text" class="form__input" autofocus placeholder="Input Your Peptide Here" :maxlength=100 width="400px">
+  </div>
+  <button class="form__button" type="submit">Get My Results</button>
+            <p class="form__text">
+            </p>
+</div>
+
 </template>
 
+
+
 <script>
+
+
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
