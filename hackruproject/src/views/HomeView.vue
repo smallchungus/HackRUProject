084772642d7abf5eced3c8ce2aa27ae3a362202d
aryan-shah27 <145288290+aryan-shah27@users.noreply.github.com>
2023-10-07 21:54:14 -0400

    Update HomeView.vue

--- hackruproject/src/views/HomeView.vue
+++ hackruproject/src/views/HomeView.vue
@@ -2,11 +2,13 @@
   <div class="home-page">
     <h1>Modern Genome App</h1>
     <p>Upload your genomic sequence to visualize it and make new discoveries.</p>
-
-    <div class="input-group">
-      <input type="file" id="file-upload" />
-      <input type="text" id="text-input" />
+    <form action="check.js">
+      <div class="input-group">
+      <textarea id="protein" name="protein" rows="6" cols="50"> Enter protein chain (400 char max) </textarea>
+      <textarea id="peptide" name="peptide" rows="6" cols="50"> Enter peptide chain (100 char max) </textarea>
       <button type="submit">Confirm</button>
     </div>
+    </form>
+    
   </div>
 </template>
