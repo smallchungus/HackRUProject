fc0babb042dd1c15cafcc18e36e6491ace84b442
WillMACOS <wchen1396@gmail.com>
2023-10-08 11:17:35 -0400

    changing css styles
    changing and merging css styles

---
+++ .history/hackruproject/src/views/HomeView_20231008110830.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home-page">
+    <h1>PeptideMapper</h1>
+    <p>Upload your genomic sequence to visualize it and make new discoveries.</p>
+
+      <div class="input-group">
+      <input id = peptidetext v-model="protein" type = "text" autofocus placeholder="Input Your Protein Here" maxlength=400  rows="6" cols="50" width="400px">
+      <input id = proteintext v-model="peptide" type = "text" autofocus placeholder="Input Your Peptide Here" maxlength=100 rows="6" cols="50" width = "1000px">
+      <button type="submit" @click="checkInput">Get My Results</button>
+      </div>
+    
+  </div>
+</template> 
+
+<script>
+export default
+{
+  data()
+  {
+    return{ 
+      protein: "",
+      peptide: "",
+    }
+  },
+methods: 
+{
+ checkInput() 
+  {
+    let proLength = this.protein.length;
+    let pepLength= this.peptide.length;
+    console.log(proLength);
+    let proteinU = this.protein.toUpperCase();
+    let peptideU = this.peptide.toUpperCase();
+    for (let i = 0; i < proLength; i++) 
+    {
+      let currentChar1 = proteinU[i];
+      
+      // Check if the current character is one of the restricted letters
+      if (currentChar1 === 'B' || currentChar1 === 'U' || currentChar1 === 'X' || currentChar1 === 'Z' )
+      {
+        alert("You cannot input the letters B, U, X, or Z.");
+        return; // Stop processing if a restricted letter is found
+      }
+    }
+    for (let i = 0; i < pepLength; i++)
+    {  
+      let currentChar2 = peptideU[i];
+      if (currentChar2 === 'B' || currentChar2 === 'U' || currentChar2 === 'X' || currentChar2 === 'Z' )
+      {
+        alert("You cannot input the letters B, U, X, or Z.");
+        return; // Stop processing if a restricted letter is found
+      }
+    }
+    alert("Valid Input")
+    }
+  }
+}
+
+</script>
---
+++ .history/hackruproject/src/views/RegisterView_20231008110928.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="register">
+      <form class="form form--hidden" id="createAccount">
+            <h1 class="form__title">Register</h1>
+            <div class="form__message form__message--error"></div>
+            <div class="form__input-group">
+                <input type="text" id="signupUsername" class="form__input" autofocus placeholder="Username">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Email Address">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Confirm password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Cardholder Name">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Credit Card Number">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="month" id="start" name="start" min="2024-03" value="2024-05" />
+                <div class="form__input-error-message"></div>
+            </div>
+            <!-- <button class="form__button" @click="store">Continue</button>
+            <p class="form__text"> -->
+              <button class="form__button" type="submit">Continue</button>
+            <p class="form__text">
+            </p>
+        </form>
+    </div>
+  </template>
+  
